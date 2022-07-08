d421872136cd44d2783025ae1743dc4b76cdc783
dorye123 <shaishamai12@gmail.com>
2022-07-08 17:20:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+function Footer() {
+    return <h1>Footer</h1>
+}
+
+function Header() {
+    return <h1>Header</h1>
+}
+
+
