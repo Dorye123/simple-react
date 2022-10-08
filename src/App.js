b9b34c02bebce50ad83f55bb1560acced0303321
dorye123 <shaishamai12@gmail.com>
2022-10-08 19:05:12 +0300

    updated design of the body of the website

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import Header from './header.js';
 import Footer from './footer.js';
-import Body from './body.js';
+import WebsiteBody from './body.js';
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <Body />
+        <WebsiteBody />
         <Footer />
     </div>
   );
--- src/body.js
+++ src/body.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import './body.css';
 
-export default function Body() {
+export default function WebsiteBody() {
     return (
-        <div className='Body'>
-            <RegularStairs />
-            <CircleStairs />
+        <div className='row'>
+            <IntroductionText/>
+            <Images />
+        </div>
+    )
+}
+
+function Images() {
+    return (
+        <div>
+            <RegularStairs/>
+            <CircluarStairs/>
         </div>
     )
 }
 
 function RegularStairs() {
     return (
-        <img src='imgs/new-regular-stairs.jpg' alt='Regular stairs calculation' className='img' ></img>
+        // I can refactor this to be an image with onclick instead of a button
+        <button onClick={event =>  window.location.href='regularStairs.html'} className='image regular'></button>
     )
 }
 
-function CircleStairs() {
+function CircluarStairs() {
     return (
-        <img src='imgs/circle-stairs.jpg' alt='Circle stairs calculation' className='img' ></img>
+        // I can refactor this to be an image with onclick instead of a button
+        <button onclick={event =>  window.location.href='circleStairs.html'} className='image circluar'></button>
+    )
+}
+
+function IntroductionText() {
+    return (
+        <div className='IntroductionText'>
+            <h1>Welcome to Advanced Stairs Calculator</h1>
+            <p>Our calculators are programmed for internal designers and architects that require high level stairs calculation.</p>
+        </div>
     )
 }
