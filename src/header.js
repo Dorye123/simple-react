1682a3b216673ad710404eb5c580e02220c8cfa3
dorye123 <shaishamai12@gmail.com>
2022-10-08 19:59:25 +0300

    wip on header, adding colors and the text for the definitions in the header

--- src/header.js
+++ src/header.js
@@ -9,13 +9,51 @@ export default function Header() {
 
 function ListOfTerminology() {
     return (
+        // instead of p element use detail and for the data about each we will have a summary element https://dev.to/ruppysuppy/7-cool-html-elements-nobody-uses-330c
+        // use sosiv.io website as an example
         <div className='TerminologyDiv'>
-            <ul>
-                <li><p className="TerminationsText">Rise and Tread</p></li>
-                <li><p className="TerminationsText">Podest</p></li>
-                <li><p className="TerminationsText">Regulations</p></li>
+            <ul> 
+                <li>
+                    <p className="TerminationsText">Rise and Tread</p>
+                    <RiseAndTread/>
+                    </li>
+                <li>
+                    <p className="TerminationsText">Podest</p>
+                    <Podest/>
+                    </li>
+                <li>
+                    <p className="TerminationsText">Regulations</p>
+                    <Regulations/>
+                    </li>
             </ul>
         </div>
+    )
+}
+
+function RiseAndTread() {
+    return (
+        // could put here div, but currently will not. Div could be use to create the box and around the text.
+        <pre className='hiddenText' >
+            Run - The height of the ascent to the next step.
+            Tread - The depth of the next step to allow proper and safe footing.
+        </pre>
+    )
+}
+
+function Podest() {
+    return (
+        <pre className='hiddenText' >
+            Podest is a step on which you can "rest" when climbing the stairs.
+        </pre>
+    )
+}
+
+function Regulations() {
+    return (
+        <pre className='hiddenText' >
+            Israely regulations - 
+            American regulations - 
+        </pre>
     )
 }
 
